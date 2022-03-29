fe37ddff999a50d13363edfb1992633475150b52
Gregory Koutsantonis <gregkoutsa@gmail.com>
2022-03-29 21:09:43 +1100

    done for tuesday

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  rest1 = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Faker::Restaurant.type
+  )
+  puts 'created one restaurant'
+end
+
+# 4.times do
+#   Review.new(
+#     content: Faker::Lorem.paragraph(sentence_count: 2),
+#     rating: Faker::Number.within(range: 0..5)
+#   )
+# end
