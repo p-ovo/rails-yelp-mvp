50cd8dcceb287ce3aa65c12e4b9517d1312dde39
Gregory Koutsantonis <gregkoutsa@gmail.com>
2022-03-31 19:13:12 +1100

    all greens

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,4 +7,21 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating, :phone_number, :category)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, presence: true, acceptance: { accept: [0, 1, 2, 3, 4, 5] }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: [0, 1, 2, 3, 4, 5]
 end
